6480ad855fa94d62404509717c05ec038d514c89
Carlos Beltran <imthatcarlos@gmail.com>
2019-01-18 11:29:20 -0500

    add 0x tools for profiling, coverage, and reverts

--- truffle.js
+++ truffle.js
@@ -1,9 +1,49 @@
+const ProviderEngine = require("web3-provider-engine")
 const HDWalletProvider = require("truffle-hdwallet-provider");
 const NonceTrackerSubprovider = require("web3-provider-engine/subproviders/nonce-tracker");
+
+const { TruffleArtifactAdapter } = require('@0x/sol-trace');
+const { ProfilerSubprovider } = require("@0x/sol-profiler");
+const { CoverageSubprovider } = require("@0x/sol-coverage");
+const { RevertTraceSubprovider } = require("@0x/sol-trace");
+
+const projectRoot = "";
+const solcVersion = "0.5.0";
+const defaultFromAddress = "0x5409ed021d9299bf6814279a6a1411a7e866a631"; // from 0xorg/devnet docker
+const isVerbose = true;
+const artifactAdapter = new TruffleArtifactAdapter(projectRoot, solcVersion);
+const provider = new ProviderEngine();
 
 require('dotenv').config();
 require('@babel/register');
 require('@babel/polyfill');
+
+const modeIdx = process.argv.indexOf('--mode');
+const mode = modeIdx != -1 ? process.argv[modeIdx + 1] : 'development';
+
+if (mode === "profile") {
+  global.profilerSubprovider = new ProfilerSubprovider(
+    artifactAdapter,
+    defaultFromAddress,
+    isVerbose
+  );
+  global.profilerSubprovider.stop();
+  provider.addProvider(global.profilerSubprovider);
+} else if (mode === "coverage") {
+  global.coverageSubprovider = new CoverageSubprovider(
+    artifactAdapter,
+    defaultFromAddress,
+    isVerbose
+  );
+  provider.addProvider(global.coverageSubprovider);
+} else if (mode === "trace") {
+  const revertTraceSubprovider = new RevertTraceSubprovider(
+    artifactAdapter,
+    defaultFromAddress,
+    isVerbose
+  );
+  provider.addProvider(revertTraceSubprovider);
+}
 
 module.exports = {
   mocha: {
@@ -16,25 +56,29 @@ module.exports = {
   // },
   networks: {
     development: {
+      provider,
       host: 'localhost',
       port: 8545,
       network_id: '*',
-      gasPrice: 4300000000 // based on https://ethgasstation.info/
+      gasPrice: 4300000000
     },
     ropsten: {
       provider: function() {
-        return new HDWalletProvider(process.env.MNEMONIC, "https://ropsten.infura.io/" + process.env.INFURA_API_KEY)
+        provider.addProvider(new HDWalletProvider(process.env.MNEMONIC, "https://ropsten.infura.io/" + process.env.INFURA_API_KEY));
+        provider.start();
+        provider.send = provider.sendAsync.bind(provider);
+        return provider;
       },
       network_id: '3',
       gas: 4500000
     },
     mainnet: {
       provider: function() {
-        var wallet = new HDWalletProvider(process.env.MNEMONIC, "https://mainnet.infura.io/" + process.env.INFURA_API_KEY);
-        var nonceTracker = new NonceTrackerSubprovider();
-        wallet.engine._providers.unshift(nonceTracker);
-        nonceTracker.setEngine(wallet.engine);
-        return wallet;
+        provider.addProvider(new HDWalletProvider(process.env.MNEMONIC, "https://mainnet.infura.io/" + process.env.INFURA_API_KEY));
+        provider.addProvider(new NonceTrackerSubprovider());
+        provider.start();
+        provider.send = provider.sendAsync.bind(provider);
+        return provider;
       },
       network_id: '1',
       port: 8546,
