bdf8aa775fb0e7c69894d8b302e8ba0e25ab7083
Carlos Beltran <imthatcarlos@gmail.com>
2019-03-01 10:22:40 -0500

    cleanup deploy migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,10 +3,6 @@ const AssetRegistry = artifacts.require("./AssetRegistry");
 const TToken = artifacts.require("./TToken");
 const PTToken = artifacts.require("./PTToken");
 
-// linking lib...
-// const contract = require("truffle-contract");
-// const Web3 = require("web3");
-
 var fs = require("fs");
 var path = require("path");
 
@@ -14,24 +10,8 @@ module.exports = function(deployer, _network, _accounts) {
   var networkIdx = process.argv.indexOf("--network");
   var network = networkIdx != -1 ? process.argv[networkIdx + 1] : "development"
 
-  // if (network == "mainnet") {
-  //   return;
-  // }
-
   var filePath = path.join(__dirname, "./../contracts.json");
   var data = JSON.parse(fs.readFileSync(filePath, "utf8"));
-
-  // linking lib...
-  // // hacky workaroung to include compiled bytecode for Array256Lib
-  // var provider = new Web3.providers.HttpProvider("http://localhost:8545");
-  // var arrayLibJSON = require("./../node_modules/ethereum-libraries-array-utils/build/contracts/Array256Lib.json")
-  // const Array256Lib = contract(arrayLibJSON);
-  // Array256Lib.setProvider(provider);
-  //
-  // // only overwriting the deployed code if deploying locally
-  // const overwrite = network === "mainnet" ? false : true;
-  // deployer.deploy(Array256Lib, { overwrite: overwrite, from: _accounts[0] });
-  // deployer.link(Array256Lib, Main);
 
   // deploy contract
   deployer.deploy(TToken).then(() => {
